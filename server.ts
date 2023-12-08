bd2c259d1be3c8404e5a5b8b090fc436934bf3e5
axon4 <65050809+axon4@users.noreply.github.com>
2023-12-08 23:45:31 +0000

    render & serve template with valid information

--- server.ts
+++ server.ts
@@ -3,20 +3,26 @@ import dotEnv from 'dotenv';
 import { fastify } from 'fastify';
 import formBody from '@fastify/formbody';
 import staticFiles from '@fastify/static';
+import axios from 'axios';
+import { z } from 'zod';
 import nunJucks from 'nunjucks';
+import geoCode from './geoCode';
+import weather from './weather';
 
 dotEnv.config();
 
 const environment = process.env.NODE_ENV;
-
-const templates = new nunJucks.Environment(new nunJucks.FileSystemLoader('templates'));
-
 const server = fastify({logger: true});
+const templates = new nunJucks.Environment(new nunJucks.FileSystemLoader('templates'));
 
 server.register(formBody);
 server.register(staticFiles, {root: path.join(__dirname, '../../dist')});
 
-const getWeatherCodeImage = (code: number): string => {
+const locationSchema = z.object({
+	query: z.string()
+});
+
+function getWeatherCodeImage(code: number): string {
 	switch (code) {
 		case 0: return '/images/clear.svg';
 
@@ -77,3 +83,42 @@ const getWeatherCodeImage = (code: number): string => {
 		default: return '/images/information.svg';
 	};
 };
+
+server.get('/', async (request, response) => {
+	try {
+		const { query } = locationSchema.parse(request.query);
+
+		const location = await geoCode(axios, query);
+		const foreCast = await weather(axios, location.latitude, location.longitude);
+
+		const rendered = templates.render('weather.njk', {
+			environment,
+			location: location.name,
+			currentDateTime: (new Date()).toLocaleDateString(),
+			weather: {
+				...foreCast,
+				condition: foreCast.condition,
+				conditionImage: getWeatherCodeImage(foreCast.weathercode),
+				lowTemperature: foreCast.lowestTemperature,
+				highestTemperature: foreCast.highestTemperature
+			}
+		});
+
+		await response.header('Content-Type', 'text/html; charset=UTF-8').send(rendered);
+	} catch (error) {
+		console.error(error);
+
+		const rendered = templates.render('home.njk', { environment });
+
+		await response.header('Content-Type', 'text/html; charset=UTF-8').send(rendered);
+	};
+});
+
+(async function (): Promise<void> {
+	try {
+		await server.listen({port: 3000});
+	} catch (error) {
+		server.log.error(error);
+		process.exit(1);
+	};
+})();
